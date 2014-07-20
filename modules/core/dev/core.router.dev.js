c1875dbd26a25271e96845895b646d09891368c2
Ricky Hurtado <ricky@aelogica.com>
2014-07-20 22:18:05 +0800

    Remove the routes that has been used in demos

--- modules/core/dev/core.router.dev.js
+++ modules/core/dev/core.router.dev.js
@@ -1,6 +1,6 @@
 /**
  * core.router.dev.js
- * 
+ *
  * @package   CoreRouter
  * @category  Router
  * @version   1.0
@@ -21,93 +21,33 @@ define([], function()
     {
       // Make the route link clickable
       I.Core.Controller.enableObjectRoute(['main']);
-      
+
       this.checkPage = I.Core.Controller.checkPage();
-      
+
       console.log('Backbone.Router.CoreRouter has been initialized.');
     },
-    
+
     /**
      * Register routes
      */
     routes :
     {
-      ''                     : 'index',
-      'router-and-view'      : 'message',
-      'model-and-collection' : 'data',
-      'login'                : 'login',
-      'register'             : 'register',
-      'server-sent-event'    : 'sse',
-      ':static_page'         : 'staticPage'
+      ''             : 'index',
+      ':static_page' : 'staticPage'
     },
-    
+
     /**
      * Index route function
      */
     index : function()
     {
       console.log('[Route] default');
-      
+
       clearTimeout(this.checkPage);
       I.Core.View.renderCorePage();
       I.Core.Controller.showContent( { el : '#index-content', rel : '.sub-content[id]' } );
     },
-    
-    /**
-     * Message subroute function
-     */
-    message : function()
-    {
-      if (!I.Core.Router.Message && I.Message)
-      {
-        I.Core.Router.Message = I.Message.Router;
-      }
-    },
-    
-    /**
-     * Data subroute function
-     */
-    data : function()
-    {
-      if (!I.Core.Router.Data && I.Data)
-      {
-        I.Core.Router.Data = I.Data.Router;
-      }
-    },
-    
-    /**
-     * Login subroute function
-     */
-    login : function()
-    {
-      if (!I.Core.Router.Login && I.Login)
-      {
-        I.Core.Router.Login = I.Login.Router;
-      }
-    },
-    
-    /**
-     * Register subroute function
-     */
-    register : function()
-    {
-      if (!I.Core.Router.Login && I.Registration)
-      {
-        I.Core.Router.Login = I.Registration.Router;
-      }
-    },
-    
-    /**
-     * Server-Sent Event subroute function
-     */
-    sse : function()
-    {
-      if (!I.Core.Router.SSE && I.ServerSentEvent)
-      {
-        I.Core.Router.SSE = I.ServerSentEvent.Router;
-      }
-    },
-    
+
     /**
      * Render and display the static page
      */
@@ -122,11 +62,11 @@ define([], function()
       {
         static_page = 'page-not-found';
       }
-      
+
       I.Core.View.renderStaticPage(static_page);
       I.Core.Controller.showContent( { el : '#' + static_page + '-content', rel : '.sub-content[id]' } );
     }
   });
-  
+
   return CoreRouter;
 });
